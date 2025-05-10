2dc13b18bb572063bab39ac3fb3b51248225e02f
Piyush Pawar <142436757+piyushpawar29@users.noreply.github.com>
2025-05-10 12:35:23 +0530

    Fix CORS for frontend

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express=require('express');
 const app=express();
 const port=process.env.PORT || 3000;
+const cors = require('cors');
+
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://api-deploy-frontend.vercel.app'],
+  methods: ['GET', 'POST'],
+}));
+
 
 app.use(express.json());
 
